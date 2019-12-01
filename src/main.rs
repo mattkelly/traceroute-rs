d1af0d3fe10f409e1fb617cc05af2529ee316589
Matt Kelly <matthew.joseph.kelly@gmail.com>
2019-12-01 09:03:07 -0500

    Experiments

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{self, NetworkInterface};
-use pnet::packet::icmp::IcmpPacket;
+use pnet::packet::icmp::{IcmpPacket, IcmpTypes};
 use std::net::UdpSocket;
 
 fn main() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("0.0.0.0:0")?;
-    let address = "8.8.8.8:0";
-    socket.connect(address).expect("failed to connect");
-
-    socket.set_ttl(1)?;
-
-    let buf = [0; 10];
-    socket.send(&buf).expect("failed to send");
-
-    let interface = &datalink::interfaces()[0];
-    let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+    let interface = &datalink::interfaces()[1];
+    println!("interface: {:?}", interface);
+    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => panic!("Unhandled channel type"),
         Err(e) => panic!(
@@ -23,14 +15,31 @@ fn main() -> std::io::Result<()> {
         ),
     };
 
-    match rx.next() {
-        Ok(packet) => {
-            let packet = IcmpPacket::new(packet).unwrap();
+    loop {
+        let socket = UdpSocket::bind("0.0.0.0:0")?;
+        let address = "192.168.1.1:33434";
 
-            println!("packet: {:?}", packet);
-        }
-        Err(e) => {
-            panic!("An error occurred while reading: {}", e);
+        socket.set_ttl(1)?;
+
+        let buf = [0; 1];
+        socket.connect(address).expect("failed to connect");
+        socket.send(&buf).expect("failed to send");
+
+        match rx.next() {
+            Ok(packet) => {
+                let packet = IcmpPacket::new(packet).unwrap();
+
+                let icmp_type = packet.get_icmp_type();
+                let checksum = packet.get_checksum();
+                println!("icmp_type: {:?}", icmp_type);
+                println!("checksum: {:?}", checksum);
+                if icmp_type == IcmpTypes::DestinationUnreachable {
+                    println!("packet: {:?}", packet);
+                }
+            }
+            Err(e) => {
+                panic!("An error occurred while reading: {}", e);
+            }
         }
     }
 
